06d1bb93359119aaf10d662337a9f587a582553f
Hermeson Alves <alveshermeson84@gmail.com>
2024-11-07 08:26:32 -0300

    fix: add click event in banner

---
+++ src/js/banner.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let currentIndex = 0;
+    const images = document.querySelectorAll('.banner img');
+    const totalImages = images.length;
+
+    if (totalImages === 0) {
+        console.error('Nenhuma imagem encontrada dentro do elemento .banner');
+        return;
+    }
+
+    function showNextImage() {
+        images[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex + 1) % totalImages;
+        images[currentIndex].classList.add('active');
+    }
+
+    const banner = document.querySelector('.banner');
+    if (!banner) {
+        console.error('Elemento .banner não encontrado');
+        return;
+    }
+
+    banner.addEventListener('click', showNextImage);
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let currentIndex = 0;
-    const images = document.querySelectorAll('.banner img');
-    const totalImages = images.length;
+// document.addEventListener('DOMContentLoaded', function() {
+//     let currentIndex = 0;
+//     const images = document.querySelectorAll('.banner img');
+//     const totalImages = images.length;
 
-    function showNextImage() {
-        images[currentIndex].classList.remove('active');
-        currentIndex = (currentIndex + 1) % totalImages;
-        images[currentIndex].classList.add('active');
-    }
+//     if (totalImages === 0) {
+//         console.error('Nenhuma imagem encontrada dentro do elemento .banner');
+//         return;
+//     }
 
-    const banner = document.querySelector('.banner');
-    banner.addEventListener('click', showNextImage);
-});
+//     function showNextImage() {
+//         images[currentIndex].classList.remove('active');
+//         currentIndex = (currentIndex + 1) % totalImages;
+//         images[currentIndex].classList.add('active');
+//     }
+
+//     const banner = document.querySelector('.banner');
+//     if (!banner) {
+//         console.error('Elemento .banner não encontrado');
+//         return;
+//     }
+
+//     banner.addEventListener('click', showNextImage);
+// });
 
 
 // Rolagem categorias de cursos;
